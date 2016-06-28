2f8df7be19b2bc372061f6ba3724b4ce6272b2a1
Giorgos Bamparopoulos <gbamparop@gmail.com>
2016-06-28 14:57:50 +0300

    Change port to 8083

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var allSessionsList = {},
   allStreamChannelSettings = {};
 
 // Create a WebSocket server 
-var wss = new WebSocketServer({ port: 8080 });
+var wss = new WebSocketServer({ port: 8083 });
 
 wss.on('connection', function connection(ws) {
 

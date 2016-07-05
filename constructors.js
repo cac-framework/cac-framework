13131fc13a2abe53e7c92a892368ba26a5525590
Evdokimos Konstantinidis <evdokimosk@gmail.com>
2016-07-05 13:02:48 +0300

    buscribe unsubscribe fixes

--- constructors.js
+++ constructors.js
@@ -172,7 +172,6 @@ function WebSocketDeviceSession() {
 
     for (var i = 0; i < exceptionsLength; i++) {
       var exception = this.receiveExceptions[i];
-<<<<<<< HEAD
       if (exception.deviceType !== deviceType.unknown){
          if(exception.deviceType !== deviceException.deviceType)
             continue;
@@ -183,14 +182,6 @@ function WebSocketDeviceSession() {
       }
       index = i;
     }
-=======
-      if ((exception.deviceType === deviceType.unknown) && (exception.deviceType === deviceException.deviceType)) {
-        continue;
-      }
-      index = i;
-    }
-
->>>>>>> ab5f90d94c9c504b34950b5943e7c813a0cdcad2
     return index;
   }
 
